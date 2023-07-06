632fd0c9a65910d972d0045d1ecc8520835b81d1
nickcrazyman <nickfuller2000@gmail.com>
2023-07-06 17:48:14 -0400

    App Finishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
+import { useState } from 'react'
 // Write your Color component here
-
-const App = () => {
+const Color = ({color, setSelectedColor}) => {
   return (
-    <div id="container">
-      <div id="navbar">
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
+}
+
+<div id="colors-list">
+  <Color color="red"/>
+  <Color color="blue"/>
+  <Color color="green"/>
+</div>
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
+  return (
+    <div id="navbar">
+    <div>Currently selected: </div>
+    <div className={selectedColor}>{selectedColor}</div>
+
+  <div className={selectedColor}>{selectedColor}</div>
+  <div id="colors-list">
+  <Color color="red" setSelectedColor={setSelectedColor}/>
+  <Color color="blue" setSelectedColor={setSelectedColor}/>
+  <Color color="green" setSelectedColor={setSelectedColor}/>
+</div>
+
+      {/* <div id="navbar">
         <div>Currently selected: </div>
+        <div id="colors-list">
+          <Color color="red" />
+          <Color color="blue" />
+          <Color color="green" />
+        </div>
         <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      </div> */}
+      <div id="colors-list">
+  <Color />
+  <Color />
+  <Color />
+  </div>
     </div>
   );
 };
